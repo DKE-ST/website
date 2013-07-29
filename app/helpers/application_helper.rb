1fe827443c51dd104b12122f148e0741f345c872
BroPorn <broporn@DEKE-LAMBDA.MIT.EDU>
2013-07-29 17:21:05 -0400

    fixed bug in in_group helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,15 +15,13 @@ module ApplicationHelper
   
   def in_group(group_name)
     if Rails.env.production?
-      groups=Array();
-      if !File.exists?('/etc/apache2/dke_users.groups')
-        return true;
-      end
+      groups=Array([])
       File.open('/etc/apache2/dke_users.groups').each_line do |line|
         if (line[0]!="#" && line[0]!="\n")
           ofset = line.index(':')
-          if line.index(ENV['REMOTE_USER']).begin(ofset)
-            groups << line[0..ofset]
+          user_list = line[ofset..-1]
+          if user_list.index(ENV['REMOTE_USER'])
+            groups << line[0..ofset-1]
           end
         end
       end
