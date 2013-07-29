6333feefabe42084e453d4a203a8c284ca061e5f
Justin Wallace <wallace4@mit.edu>
2013-07-29 17:34:31 -0400

    Made code more human readable (hopefully)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,26 @@
 module ApplicationHelper
 
   def get_name(username)
-    if Rails.env.production?
-      tmp = BrothersPersonal.find_by( uname: username)
-      if tmp
-        return "#{tmp.first_name} #{tmp.last_name}"
-      else
-        return username
-      end
-     else
-       return 'Justin Wallace'
-     end
+    return 'Justin Wallace' if !Rails.env.production?  
+    tmp = BrothersPersonal.find_by( uname: username)
+    return "#{tmp.first_name} #{tmp.last_name}" if tmp
+    return username
   end
   
   def in_group(group_name)
-    if Rails.env.production?
-      groups=Array([])
-      File.open('/etc/apache2/dke_users.groups').each_line do |line|
-        if (line[0]!="#" && line[0]!="\n")
-          ofset = line.index(':')
-          user_list = line[ofset..-1]
-          if user_list.index(ENV['REMOTE_USER'])
-            groups << line[0..ofset-1]
-          end
+    return true if !Rails.env.production?
+    groups=Array([])
+    File.open('/etc/apache2/dke_users.groups').each_line do |line|
+      if (line[0]!="#" && line[0]!="\n")
+        ofset = line.index(':')
+        user_list = line[ofset..-1]
+        if user_list.index(ENV['REMOTE_USER'])
+          groups << line[0..ofset-1]
         end
       end
-      if groups.include? group_name
-        return true
-      else
-        return false
-      end
-     else
-       return true
-     end
+    end
+    return true if groups.include? group_name
+    return false
   end
   
   
