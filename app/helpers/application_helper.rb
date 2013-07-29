c8274e1c345df4d30a2214e1a2a597eb9ced87ff
Justin Wallace <wallace4@mit.edu>
2013-07-29 16:11:21 -0400

    Refactored navbar Made navbar content group specific

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,42 @@
 module ApplicationHelper
 
   def get_name(username)
-    tmp = BrothersPersonal.find_by( uname: username)
-    if tmp
-      return "#{tmp.first_name} #{tmp.last_name}"
-    else
-      return username
-    end
+    if Rails.env.production?
+      tmp = BrothersPersonal.find_by( uname: username)
+      if tmp
+        return "#{tmp.first_name} #{tmp.last_name}"
+      else
+        return username
+      end
+     else
+       return 'Justin Wallace'
+     end
   end
+  
+  def in_group(group_name)
+    if Rails.env.production?
+      groups=Array();
+      if !File.exists?('/etc/apache2/dke_users.groups')
+        return true;
+      end
+      File.open('/etc/apache2/dke_users.groups').each_line do |line|
+        if (line[0]!="#" && line[0]!="\n")
+          ofset = line.index(':')
+          if line.index(ENV['REMOTE_USER']).begin(ofset)
+            gtoups << line[0..ofset]
+          end
+        end
+      end
+      if groups.include? group_name
+        return true
+      else
+        return false
+      end
+     else
+       return true
+     end
+  end
+  
+  
 
 end
