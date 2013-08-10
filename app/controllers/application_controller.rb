954bd695a73450792733121ad5c5e709ea962494
Justin Wallace <wallace4@mit.edu>
2013-08-10 15:42:39 -0400

    made @me global, don't expect it to work correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :set_user
+  
+  def set_user
+    @me = Apache.new(ENV['REMOTE_USER'])
+    @me = Apache.new("wallace4") unless ENV["SERVER_NAME"] == "bruiser.mit.edu"
+  end
+  
 end
