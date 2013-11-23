8c19dfc979a749738074316c5065c8a8c658aca0
Justin <wallace4@justin-XPS-L502X.(none)>
2013-11-23 06:12:06 -0500

    Created model and controller methods, and view for simple pages with text (President's letter , about page ...)

--- app/assets/javascripts/chapter_public.js.coffee
+++ app/assets/javascripts/chapter_public.js.coffee
@@ -1,3 +1,24 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+resizeIt = ->
+  textarea = $("#chapter_public_content")
+  lines = textarea.val().split("\n")
+  width = textarea.width()/6.5
+  height = 1
+  i = 0
+  while i < lines.length
+    linelength = lines[i].length
+    height += Math.floor(linelength / width)  if linelength >= width
+    i++
+  height += lines.length
+  textarea.height height * 20
+
+$(document).keypress (e) ->
+  resizeIt()
+  
+$(document).ready ->
+  resizeIt()
+  
+#Event.observe "chapter_public_content", "keydown", resizeIt # you could attach to keyUp, etc if keydown doesn't work
--- app/controllers/chapter_public_controller.rb
+++ app/controllers/chapter_public_controller.rb
@@ -1,5 +1,7 @@
 class ChapterPublicController < ApplicationController
   
+  before_action :correct_user, only: [:edit, :update]
+  
   def contact
     @officers = Hash.new
     position_map =  BrothersDke.select("cur_pos, uname")
@@ -9,6 +11,34 @@ class ChapterPublicController < ApplicationController
         year = BrothersMit.find_by(uname: position.uname).year.to_s[2..3]
         @officers[position.cur_pos] = {uname: position.uname, full_name: name, year: year}
       end
+    end
+  end
+  
+  def show
+    @content = ChapterPublic.find_by(pname: params[:id])
+  end
+  
+  def edit
+    @content = ChapterPublic.find_by(pname: params[:id])
+  end
+  
+  def update
+    @content = ChapterPublic.find_by(pname: params[:id])
+    if @content.update_attributes(params.require(:chapter_public).permit(:title, :pname, :content))
+      flash[:success] = "Information updated"
+      redirect_to chapter_public_url
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def correct_user
+    
+    unless @me.in_group(ChapterPublic.find_by(pname: params[:id]).user)
+      flash[:error] = "You do not have acess to this page"
+      redirect_to chapter_public_url
     end
   end
   
