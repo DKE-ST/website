31975bf5dadee8819664b886edbb8a69ee2b9adb
Justin Wallace <wallace4@justin-pc.(none)>
2014-02-02 09:44:47 -0500

    Updated table for house points

--- config/sql files/house_points.sql
+++ config/sql files/house_points.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 03, 2014 at 09:52 PM
--- Server version: 5.5.34
+-- Generation Time: Feb 02, 2014 at 09:44 AM
+-- Server version: 5.5.35
 -- PHP Version: 5.3.10-1ubuntu3.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -26,14 +26,38 @@ SET time_zone = "+00:00";
 -- Table structure for table `house_points`
 --
 
-DROP TABLE IF EXISTS `house_points`;
-
 CREATE TABLE IF NOT EXISTS `house_points` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uname` varchar(8) NOT NULL,
   `position` text NOT NULL,
   `value` int(11) NOT NULL,
   `comment` text NOT NULL,
-  `start_date` date NOT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `house_points`
+--
+
+INSERT INTO `house_points` (`id`, `uname`, `position`, `value`, `comment`, `date`) VALUES
+(2, 'pjmaster', 'beta', 10, 'Fake Pts', '2014-02-02'),
+(3, 'esparrs', 'beta', 5, 'Fake Pts', '2014-01-24'),
+(4, 'jmcunnin', 'beta', 5, 'Fake Pts', '2014-01-24'),
+(5, 'wallace4', 'sigma', 5, 'Fake Pts', '2014-01-24'),
+(6, 'pjmaster', 'sigma', 5, 'Fake Pts', '2014-01-24'),
+(7, 'esparrs', 'sigma', 5, 'Fake Pts', '2014-01-24'),
+(8, 'jmcunnin', 'sigma', 5, 'Fake Pts', '2014-01-24'),
+(9, 'wallace4', 'epsilon', 5, 'Fake Pts', '2014-01-24'),
+(10, 'pjmaster', 'epsilon', 5, 'Fake Pts', '2014-01-24'),
+(11, 'esparrs', 'epsilon', 5, 'Fake Pts', '2014-01-24'),
+(12, 'jmcunnin', 'epsilon', 5, 'Fake Pts', '2014-01-24'),
+(13, 'wallace4', 'delta', 5, 'Fake Pts', '2014-01-24'),
+(14, 'pjmaster', 'delta', 5, 'Fake Pts', '2014-01-24'),
+(15, 'esparrs', 'delta', 5, 'Fake Pts', '2014-01-24'),
+(16, 'jmcunnin', 'delta', 5, 'Fake Pts', '2014-01-24'),
+(17, 'wallace4', 'beta', -3, 'Missed House Meeting - 10/10', '2014-01-28');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
