6c22fb95717323e6ecedcb548a252bb31c86fa41
Justin Wallace <wallace4@justin-pc.(none)>
2014-02-06 06:10:50 -0500

    -Made val in Settings model a string

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,11 +1,11 @@
 class Settings < ActiveRecord::Base
   
   def self.debug?
-    return Settings.find("debug").val == 1
+    return Settings.find("debug").val.to_i == 1
   end
   
   def self.mode?(num)
-    return Settings.find("server").val == num
+    return Settings.find("server").val.to_i == num
   end
   
 end
