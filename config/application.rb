f651ab256bdca971fbad78fda49840ec69e4fdee
Justin Wallace <wallace4@justin-pc.(none)>
2014-03-19 22:21:54 -0400

    -Potential editor fix

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module RailsSite
     
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/*)
     config.autoload_paths += ['app/models/brothers','app/models/users']
   end
 end
