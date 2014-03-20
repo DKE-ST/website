c8a958b3ec520e834ecf521fbbf3931dabca1308
Justin Wallace <wallace4@justin-pc.(none)>
2014-03-19 22:11:58 -0400

    -Hopeful fix

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module RailsSite
     # config.i18n.default_locale = :de
     
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.autoload_paths += ['app/models/brothers','app/models/users']
   end
