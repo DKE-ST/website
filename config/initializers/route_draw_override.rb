bf658e7b527d6a894dd4260c6d951a952271ceb0
Justin Wallace <wallace4@mit.edu>
2014-10-11 04:38:34 -0400

    -Removed unnecessary file

--- config/initializers/route_draw_override.rb
+++ config/initializers/route_draw_override.rb
@@ -1,6 +1,13 @@
 Rails::Application::RoutesReloader.class_eval do
   def load_paths
     paths.each { |path| load(path) }
-    load(Rails.root.join('config','routes_public_pages.rb'))
+    
+    ####### Loads Chapter Public controller to root at very end ########
+    Rails.application.routes.draw do 
+      scope module: 'chapter' do
+        match '/contact', to: 'public_pages#contact' , via: :get
+        resources :public_pages, path: '/'
+      end
+    end
   end
 end
--- config/routes_public_pages.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  
-  scope module: 'chapter' do
-    match '/contact', to: 'public_pages#contact' , via: :get
-    resources :public_pages, path: '/'
-  end
-end
