4987ed70dcfd926c5bd5cad7351ef0de3db6bbc3
Justin Wallace <wallace4@mit.edu>
2014-10-11 16:12:18 -0400

    -Dummy app for rake migrations

---
+++ plugins/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '8c9c7d81c7db4f27ded5c01193bd7140ef7e86d8586000e97c4341528fcced8bf1f1807c8ebc8cde503c1176f405c9d5ec54478f4187a9eda88b3baec5a92804'
