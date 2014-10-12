51df86acd9ac561c5ae512438f728bceebd0c87a
Justin Wallace <wallace4@mit.edu>
2014-10-12 00:42:15 -0400

    -Fixed bug with selectable

--- app/assets/javascripts/chapter/officers.js
+++ app/assets/javascripts/chapter/officers.js
@@ -1,9 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready( function() {
-	$( "#sortable" ).sortable({ tolerance: "pointer" });
-	//$( "#sortable" ).disableSelection();
-});
 
 $( document ).on('submit', '.update_contacts', function( event ) {
 	var form = $(".update_contacts");
