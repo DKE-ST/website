26112d17324078ec2638cfb767a8713205fe8675
Justin Wallace <wallace4@mit.edu>
2014-10-16 15:19:11 -0400

    -Modified type for time entry in db

--- plugins/epsilon/db/migrate/20141011195555_create_epsilon_e_sheets.rb
+++ plugins/epsilon/db/migrate/20141011195555_create_epsilon_e_sheets.rb
@@ -3,7 +3,7 @@ class CreateEpsilonESheets < ActiveRecord::Migration
     create_table :epsilon_e_sheets do |t|
       t.belongs_to :dke_info
       t.date :date
-      t.time :time
+      t.string :time, limit: 8
       t.string :e_type, limit: 8
       t.float :value
       t.text :comment
