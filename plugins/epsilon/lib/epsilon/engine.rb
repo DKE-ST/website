21cfce6659879a9fce14778f4ecc4fd468ff8c18
Justin Wallace <wallace4@mit.edu>
2014-12-19 11:25:16 -0500

    -rake works for all now.

--- plugins/epsilon/lib/epsilon/engine.rb
+++ plugins/epsilon/lib/epsilon/engine.rb
@@ -12,6 +12,14 @@ module Epsilon
         require_dependency(c)
       end
     end
+    
+    initializer :append_migrations do | app |
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do | expanded_path |
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   
   end
 end
